73d26a7b236c0e07ec014b187ca0ad87fe3ec3db
Buendia <kenneth.buendia.code@gmail.com>
2025-03-28 17:17:33 +0900

    Create original routes
    - comment out default route/web.php
    - make WholesalingProvider including original routes path
    - defined original route in MainRoute.php and WebRoute.php
    - registered WholesalingProvider

---
+++ app/Wholesaling/Utils/MainRoute.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Wholesaling\Utils;
+
+use Illuminate\Support\Facades\Route;
+
+Route::group([], base_path('app/Wholesaling/Utils/WebRoute.php'));
