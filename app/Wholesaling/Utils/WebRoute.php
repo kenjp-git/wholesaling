5a536da74b5c0ca36d3ee213306f1aa7f43036f2
Buendia <kenneth.buendia.code@gmail.com>
2025-03-29 00:44:50 +0900

    Make Page Home
    - made Home.tsx page

--- app/Wholesaling/Utils/WebRoute.php
+++ app/Wholesaling/Utils/WebRoute.php
@@ -5,7 +5,8 @@ declare(strict_types = 1);
 namespace App\Wholesaling\Utils;
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return 'Home';
+    return Inertia::render('Home');
 });
---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+    return (
+        <div className="text-center">Wholesaling Home</div>
+    )
+}
