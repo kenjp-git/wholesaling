f3f4c0a84beef0c0e6be44f3f41c151f7fdf0c46
Buendia <kenneth.buendia.code@gmail.com>
2025-04-05 20:23:31 +0900

    Deleted component Table.tsx

--- resources/js/Components/Forms/TableInput.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function TableInput() {
-    return (
-        <input
-            className="px-1 py-0 w-full border-transparent focus:bg-yellow-100 focus:ring-0 focus:border-transparent"
-            type="text"
-        />
-    )
-}
