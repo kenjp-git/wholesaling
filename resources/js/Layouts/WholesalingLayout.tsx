95bdf9d06e84bdb575a605f3b52af09c84f2d3f0
Buendia <kenneth.buendia.code@gmail.com>
2025-04-07 23:22:00 +0900

    Improve Wholesalinglayout.tsx
    - improve style of header
    - improve the logic of automating title view

--- resources/js/Layouts/WholesalingLayout.tsx
+++ resources/js/Layouts/WholesalingLayout.tsx
@@ -1,20 +1,24 @@
 import { Head, usePage } from "@inertiajs/react";
-import { PropsWithChildren, HTMLProps } from "react";
+import { PropsWithChildren, HTMLProps, useEffect, useState } from "react";
 
 export default function WholesalingLayout({
     children
 }: PropsWithChildren<HTMLProps<HTMLElement>>) {
+    const [pageTitle, setPageTitle] = useState<string>('')
 
-    const pageData = document.getElementById('app')?.dataset.page || '{}'
-    const pageObject = JSON.parse(pageData)
-    const pageName = pageObject.component.split('/').pop()
-    const pageTitle = pageObject.props.translations.words[pageName] || import.meta.env.VITE_APP_NAME
+    useEffect(() => {
+        const pageData = document.getElementById('app')?.dataset.page || '{}'
+        const pageObject = JSON.parse(pageData)
+        const pageName = pageObject.component.split('/').pop()
+        const pageTitle = pageObject.props.translations.words[pageName] || import.meta.env.VITE_APP_NAME
+        setPageTitle((prev) => { return pageTitle })
+    }, [pageTitle])
 
     return (
         <>
             <Head title={pageTitle} />
-            <header className="h-10 bg-blue-200">
-                <h1 className="text-center text-2xl font-bold">{pageTitle}</h1>
+            <header className="w-[1024px] mx-auto bg-blue-200">
+                <h1 className="text-center text-lg my-auto">{pageTitle}</h1>
             </header>
             {children}
         </>
