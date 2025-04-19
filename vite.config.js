10ec312fc64495a25f116219615f6cdb7f914194
Buendia <kenneth.buendia.code@gmail.com>
2025-04-19 23:00:01 +0900

    Fix server error bugs
    - fixed server 500 error by adding input resources in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: [
+                'resources/js/app.tsx',
+                'resources/js/Pages/Error.tsx',
+                'resources/js/Pages/Home.tsx',
+            ],
             refresh: true,
         }),
         react(),
