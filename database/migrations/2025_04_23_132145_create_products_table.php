56b3071e7ed464fc46f186f65a414014779c0dc4
Buendia <kenneth.buendia.code@gmail.com>
2025-04-23 22:45:10 +0900

    Define products table
    - defined columns of products table

--- database/migrations/2025_04_23_132145_create_products_table.php
+++ database/migrations/2025_04_23_132145_create_products_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->id('product_code')->comment('商品コード');
+            $table->string('product_name')->comment('商品名');
+            $table->string('product_specification')->nullable()->comment('商品規格');
+            $table->string('unit')->comment('単位');
+            $table->unsignedInteger('wholesaling_price')->comment('卸売価格');
+            $table->unsignedBigInteger('gtin_code')->unique()->comment('JAN/GTINコード');
+            $table->unsignedInteger('supplier_code')->comment('仕入先コード');
+            $table->foreign('supplier_code')->references('supplier_code')->on('suppliers')->onDelete('cascade');
             $table->timestamps();
         });
     }
