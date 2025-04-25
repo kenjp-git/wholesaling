62da8a4b2c5997ca30be0c5219b8057b4f8e260c
Buendia <kenneth.buendia.code@gmail.com>
2025-04-25 23:25:00 +0900

    Refactor method resolve
    - refactor the logic of method resolve()

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import WholesalingLayout from '@/Layouts/WholesalingLayout';
+import { ReactElement } from 'react';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -12,8 +13,8 @@ createInertiaApp({
     title: (title) => title ? `${title} - ${appName}` : `${appName}`,
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true });
-        let page = pages[`./Pages/${name}.tsx`] as { default: { layout?: unknown } };
-        page.default.layout = page.default.layout ? page.default.layout : (page: React.ReactNode) => (
+        let page = pages[`./Pages/${name}.tsx`] as { default: { layout?: (page: ReactElement) => ReactElement } };
+        page.default.layout = page.default.layout || ((page: ReactElement) =>
             <WholesalingLayout children={page} pageName={appName} />
         );
         return page;
