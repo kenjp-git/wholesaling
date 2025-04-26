e26676ad29a3461709cc7622c4c83887a3a57c73
Buendia <kenneth.buendia.code@gmail.com>
2025-04-26 11:45:00 +0900

    Improve Error.tsx
    - use the default layout for Error.tsx
    - remove h1 tag
    - pass the status to the prop pageName of default layout

--- resources/js/Pages/Error.tsx
+++ resources/js/Pages/Error.tsx
@@ -1,16 +1,32 @@
+import { ReactElement } from 'react';
+import WholesalingLayout from '@/Layouts/WholesalingLayout';
 
-export default function Error({ status, message }: { status: number, message: string }) {
+const Error = function ({ status, message }: { status: number, message: string }) {
     const title = {
         403: "403",
         404: "404",
         500: "500",
         503: "503"
-    }[status]
+    }[status] as string
 
     return (
-        <div className="w-[1024px] mx-auto text-center bg-slate-50 shadow-lg">
-            <h1>{title}</h1>
-            <p>{message}</p>
-        </div>
+        <>
+            <div className="w-[1024px] mx-auto text-center bg-slate-50 shadow-lg">
+                <p>{message}</p>
+            </div>
+        </>
     )
 }
+
+Error.layout = (page: ReactElement) => {
+    return (
+        <>
+            <WholesalingLayout
+                pageName={page.props.status}
+                children={page}
+            />
+        </>
+    )
+}
+
+export default Error;
