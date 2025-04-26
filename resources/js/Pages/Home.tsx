a62a4300a5c21749b4514b2402a48de770e0636a
Buendia <kenneth.buendia.code@gmail.com>
2025-04-26 11:00:00 +0900

    Applying the default layout for Home.tsx
    - use the default layout Wholesalinglayout
    - pass the locale name of Home to default layout

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,9 +1,20 @@
+import { ReactElement } from 'react';
 import WholesalingLayout from "@/Layouts/WholesalingLayout";
 
-function Home() {
+const Home = function () {
     return (
         <div className="text-center">Wholesaling Home</div>
     )
 }
 
+Home.layout = (page: ReactElement) => {
+    const pageName = page.props.translations.words.Home
+
+    return (
+        <WholesalingLayout
+            pageName={pageName}
+            children={page}
+        />
+    )
+}
 export default Home;
